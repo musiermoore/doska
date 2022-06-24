c47d1793681b3e79367a406cf8cbd26bed0a8765
musiermoore <musiermoore@gmail.com>
2022-06-24 21:53:03 +0600

    Added the getAuthenticatedUser method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Traits\ApiResponses;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +10,16 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponses;
+
+    public function getAuthenticatedUser()
+    {
+        $user = null;
+
+        if (!empty(auth()->user())) {
+            $user = \App\Models\User::query()->find(auth()->user()->value('id'));
+        }
+
+        return $user;
+    }
 }
